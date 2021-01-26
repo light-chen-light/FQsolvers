ba8e63ab7e08d62ac4f7714e2ea044a3ac1e13f5
shinli256 <31687832+shinli256@users.noreply.github.com>
2021-01-26 03:47:18 -0800

    Update FQsolverM1.m

--- FQsolverM1.m
+++ FQsolverM1.m
@@ -1,10 +1,10 @@
 function [OptNum,OptAP] = FQsolver(AP,Contr,Mission,Reach)
 [m,n]=size(Contr);
 if not(SIZEMATCH(AP,[n 1]))
-    error(""The first input is an n*1 matrix, the second input is an m*n matrix, where n is the number of FQ."");
+    error("The first input is an n*1 matrix, the second input is an m*n matrix, where n is the number of FQ.");
 end
 if or(not(SIZEMATCH(Mission,[m 1])),not(SIZEMATCH(Reach,[m 1])))
-    error(""The second input is an m*n matrix, the third and the fourth input are m*1 matrices, where m is the number of missions considered."");
+    error("The second input is an m*n matrix, the third and the fourth input are m*1 matrices, where m is the number of missions considered.");
 end
 FQprob=optimproblem;
 Num=optimvar('Num',n,'Type','integer','LowerBound',0);
